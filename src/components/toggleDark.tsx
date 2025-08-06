9ba3fcfb2748b868cb480cec651ea080cde675f9
Ashvary1996 <ashvarygidian1996@gmail.com>
2025-08-06 18:17:51 +0530

    shadcn and accertinity added

---
+++ src/components/toggleDark.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import * as React from "react"
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+
+export function ToggleDark() {
+  const { resolvedTheme, setTheme } = useTheme()
+  const [mounted, setMounted] = React.useState(false)
+
+  React.useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null // Prevent hydration mismatch
+
+  const toggleTheme = () => {
+    setTheme(resolvedTheme === "dark" ? "light" : "dark")
+  }
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={toggleTheme}
+      className="transition hover:bg-accent hover:text-accent-foreground"
+      aria-label="Toggle theme"
+      title={resolvedTheme == "light" ? "Toggle Dark Mode" : "Toggle Light Mode"}
+    >
+      {resolvedTheme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+    </Button>
+  )
+}
+
