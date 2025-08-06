bf0d403867a3c8b765243209ad1ff23d9c85f2dd
Ashvary1996 <ashvarygidian1996@gmail.com>
2025-08-06 19:00:49 +0530

    test performance of bg grid in live

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
 
-import { NavbarMenu } from "@/components/NavbarMenu";
+
+
+
 import { ToggleDark } from "@/components/toggleDark";
+import { Boxes } from "@/components/ui/background-boxes";
+import { cn } from "@/lib/utils";
+
 
 
 
 export default function Home() {
   return (
     <>
-      {/* <NavbarMenu /> */}
-      <h1>home Page</h1>
-      <ToggleDark />
 
 
 
 
+      <div className="h-96 relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+        <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+        <Boxes />
+        <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+          Home Page
+        </h1>
+        <p className="text-center mt-2 text-neutral-300 relative z-20">
+          Toggle <ToggleDark />
+        </p>
+
+      </div>
 
 
     </>
