8ce4c0c038ecb3e140aae521bd0942a531c410ff
Ashvary1996 <ashvarygidian1996@gmail.com>
2026-02-20 20:23:32 +0530

    magic ui and acertinity added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ThreeDCardDemo from "@/components/3d-card-demo";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
@@ -5,6 +6,11 @@ export default function Home() {
     <>
       <Button>Click me</Button>
       <h1 className="text-lg bg-red-800">Hello world</h1>
+      <ThreeDCardDemo />
+      <div className="p-4 ">
+        <p>Hello World</p>
+        <span>Hover me</span>
+      </div>
     </>
   );
 }
